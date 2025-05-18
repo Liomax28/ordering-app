be3082f56cd587ebff1fe8aa01ed7b4c974f6785
Liomax28 <lionelmaxesp@gmail.com>
2025-05-17 18:16:28 -0600

    Building homepage finished

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,12 +6,12 @@ export default function Header() {
       <Link className="text-primary font-semibold text-2xl" href="">
       ST PIZZA
       </Link>
-      <nav className='flex items-center gap-4 text-gray-500 font-semibold'> 
+      <nav className='flex items-center gap-8 text-gray-500 font-semibold'> 
         <Link href={''}>Home</Link>
         <Link href={''}>Menu</Link>
         <Link href={''}>About</Link>
         <Link href={''}>Contact</Link>
-        <Link href={''} className='bg-red-500 rounded-full text-white px-8 py-2'>Login</Link>
+        <Link href={''} className='bg-primary rounded-full text-white px-8 py-2'>Login</Link>
       </nav>
     </header>
   </>
